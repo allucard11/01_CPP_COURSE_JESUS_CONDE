d12b9af0f6e8c61e2a9649977ea0a34ad516a8a1
CHARLIE <carlosgarciahdez1990@gmail.com>
2023-07-19 20:54:49 +0200

    * Example of usinf #ifdef, #define preprocessor directives. * Example to see the size of data types.

--- CPP_COURSE.cpp
+++ CPP_COURSE.cpp
@@ -2,20 +2,46 @@
 
 using namespace std;
 
-bool lolo = 5;
+#define LIS331
 
 int main() {
 
-  int x = 5;
-  int y = 7;
-
+  cout << "El tamaño de un short  ---> " << sizeof (short) << endl;
+  cout << "El tamaño de un int    ---> " << sizeof (int) << endl;
+  cout << "El tamaño de un long   ---> " << sizeof (long) << endl;
+  cout << "El tamaño de un char   ---> " << sizeof (char) << endl;
+  cout << "El tamaño de un float  ---> " << sizeof (float) << endl;
+  cout << "El tamaño de un double ---> " << sizeof (double) << endl;
+  cout << "El tamaño de un bool   ---> " << sizeof (bool) << endl;
   cout << endl;
-  cout << x + y << " " << x * y;
-  cout << endl;
 
-  if (lolo) {
+  short int num = 32767;
 
-    cout << "Me gusta C++" << endl;
+  cout << "Numero = " << num << endl;
+  num++;
+  cout << "Numero = " << num << endl;
+  num++;
+  cout << "Numero = " << num << endl;
+  num++;
+  cout << "Numero = " << num << endl;
+
+#ifdef KX124
+
+  cout << "dentro del kx124" << endl;
+
+#elif defined (LIS331)
+
+  cout << "dentro del lis331" << endl;
+
+#elif defined (KXTJ)
+
+  cout << "dentro del kxtj" << endl;
+
+#endif
+
+  for (int i = 32; i < 128; i++) {
+
+    cout << (char) i;
   }
 
   return 0;
